10a9d0cfa684bc2de02600f58d14d6ba7d7c24af
wisdomchibundu53-jpg <wisdomchibundu53@gmail.com>
2026-08-19 00:44:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
+const SUPABASE_URL = "https://mqgcfzhjapvypiwxcaj.supabase.co";
+const SUPABASE_KEY = "sb_publishable_O5zOjcU5IFCLkNzcmBf1-g_jPxzpWrt";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
+
 const form = document.getElementById("registrationForm");
 
-form.addEventListener("submit", function(event) {
+form.addEventListener("submit", async function(event) {
 
     event.preventDefault();
 
-    window.location.href = "success.html";
+    const registration = {
+        full_name: document.getElementById("name").value,
+        age: parseInt(document.getElementById("age").value),
+        phone_number: document.getElementById("phone").value,
+        email: document.getElementById("email").value,
+        course: document.getElementById("course").value,
+        preferred_start_date: document.getElementById("date").value,
+        address: document.getElementById("location").value
+    };
 
+    const { error } = await supabase
+        .from("registrations")
+        .insert([registration]);
+
+    if (error) {
+        console.error("Registration error:", error);
+        alert("Registration failed. Please try again.");
+        return;
+    }
+
+    window.location.href = "success.html";
 });
